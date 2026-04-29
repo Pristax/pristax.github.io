310a2c14e7137c8d3c0ec02fb4bc9039a3d2aa60
Pristax <pristas.honza@gmail.com>
2026-04-29 23:22:25 +0200

    0.0.2

--- src/script.js
+++ src/script.js
@@ -1,26 +1,5 @@
 const interactive = document.querySelector('.interactive');
 
-let mouseX = 0;
-let mouseY = 0;
-
-let currentX = 0;
-let currentY = 0;
-
-// plynulé sledování (smooth efekt)
-function animate() {
-    currentX += (mouseX - currentX) * 0.1;
-    currentY += (mouseY - currentY) * 0.1;
-
-    interactive.style.transform = `translate(${currentX}px, ${currentY}px)`;
-
-    requestAnimationFrame(animate);
-}
-
 window.addEventListener('mousemove', (e) => {
-    mouseX = e.clientX;
-    mouseY = e.clientY;
+    interactive.style.transform = `translate(${e.clientX}px, ${e.clientY}px)`;
 });
-
-interactive.style.transform = `translate(${currentX}px, ${currentY}px) translate(-50%, -50%)`;
-
-animate();
